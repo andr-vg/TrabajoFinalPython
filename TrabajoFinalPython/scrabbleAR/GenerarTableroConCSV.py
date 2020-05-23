85625fcb06eadb245ec894dcb1e8d05efcce1052
andr-vg <andreagoyechea@gmail.com>
2020-05-23 15:24:09 -0300

    Hice que muestre el tiempo y corte pero tiene delay

--- TrabajoFinalPython/scrabbleAR/GenerarTableroConCSV.py
+++ TrabajoFinalPython/scrabbleAR/GenerarTableroConCSV.py
@@ -3,6 +3,9 @@ import csv
 import sys
 import string
 import random
+import time # el problema: el tiempo si bien corre, tiene cierto delay, entonces si quiero que el programa finalice en
+            # 20 segundos, termina finalizando en 40 o más, porque lo hice actualizar segun los botones que va 
+            # seleccionando, hay que encontrar otra forma de actualizarlo
 
 import os
 absolute_path = os.path.dirname(os.path.abspath(__file__))      # Look for your absolute directory path
@@ -15,7 +18,7 @@ def get_consonante():   #Devuelve una consonante random
     x=random.choice('BCDFGHJKLMNÑPQRSTVWXYZ')
     return x
 
-if ("win" in sys.platform):
+if "win" in sys.platform:
     #arch = open(".\\TrabajoFinalPython\\TrabajoFinalPython\\scrabbleAR\\Datos\\tablero.csv","r")
 
     arch = open(absolute_path + '/Datos/tablero.csv',"r")       #esto lo agregue porque no me encontraba el archivo
@@ -27,7 +30,7 @@ def guardar_partida (lista):    #recibe el layout saca los botones que no son de
     guardar = lista
     guardar.pop(16)
     guardar.pop(15)
-    if ("win" in sys.platform):
+    if "win" in sys.platform:
         #arch = open(".\\TrabajoFinalPython\\TrabajoFinalPython\\scrabbleAR\\Datos\\guardado.csv","w")
 
         arch = open(absolute_path + '/Datos/guardado.csv',"w")      #esto lo agregue porque no me encontraba el archivo
@@ -87,7 +90,7 @@ def crear_layout():
     letras_valores = list(letras.values())
     letras_keys = list(letras.keys())
     fila_fichas =[sg.Button(button_text= letras_valores[i], key = letras_keys[i], size=(4, 2), button_color=('white','blue')) for i in range(7)]
-    fila_botones = [sg.Button("Confirmar",key="-c"), sg.Button("Deshacer",key="-d"),sg.Button("Terminar",key="-t"),sg.Button("Posponer",key="-p")]
+    fila_botones = [sg.Button("Confirmar",key="-c"), sg.Button("Deshacer",key="-d"),sg.Button("Terminar",key="-t"),sg.Button("Posponer",key="-p"), sg.Text(str(time.process_time()*10)+" seg", key = 'tiempo')]
     layout.append(fila_botones)
     layout.append(fila_fichas)
     return layout, letras, botones
@@ -101,15 +104,18 @@ window = sg.Window("Ventana de juego",layout)
 
 oper = ["-t","-c","-d,""-p"] #Para los botones Terminar, Confirmar, Deshacer y Posponer
 
-
-
 letras_usadas = dict()  # pares (clave, valor) de las letras seleccionadas
 
 palabra_nueva = dict()  # pares (clave, valor) de la palabra que se va formando en el tablero
 
+start_time = time.process_time()  # tiempo inicial del juego en segundos -> 0.1 = 1 seg real, supongamos que tiene que durar maximo 2.0 = 20 segs
+print(start_time)
 while True:  # Event Loop
     event, values = window.read()
     print(event, values)
+    window['tiempo'].update(str(time.process_time()*10)+" seg") # actualizo el tiempo en segundos
+    if time.process_time() > 2:  # corto la ejecucion a los "20" segundos
+        break   
     if event is None:
         break
     if event == "-p":
@@ -118,6 +124,9 @@ while True:  # Event Loop
         #Implementar
         pass
     if event == "-d":
+        window['tiempo'].update(str(time.process_time()*10)+" seg") # actualizo el tiempo en segundos
+        if time.process_time() > 2:
+            break   
         for val in letras.keys():
             window[val].update(disabled=False)
         for val in palabra_nueva:
@@ -125,14 +134,23 @@ while True:  # Event Loop
         letras_usadas = dict()
         palabra_nueva = dict()
     if event in letras.keys():
+        window['tiempo'].update(str(time.process_time()*10)+" seg") # actualizo el tiempo en segundos
+        if time.process_time() > 2:
+            break   
         box = event # letra seleccionada
         letras_usadas[box] = letras[box]
         for val in letras.keys():
             window[val].update(disabled=True) # desactivo los botones
         event, values = window.read()
+        window['tiempo'].update(str(time.process_time()*10)+" seg") # actualizo el tiempo en segundos
+        if time.process_time() > 2:
+            break   
         if event is None:
             break
         if event in botones.keys():
+            window['tiempo'].update(str(time.process_time()*10)+" seg") # actualizo el tiempo en segundos
+            if time.process_time() > 2:
+                break   
         # refresco la tabla en la casilla seleccionada con la letra elegida antes
             ind = event # casilla seleccionada
             if botones[ind] == "+":
@@ -144,3 +162,6 @@ while True:  # Event Loop
             for val in letras.keys():
                 if val not in letras_usadas.keys():
                     window[val].update(disabled=False) # refresco la tabla B
+
+start_time = time.process_time()
+print(start_time)
