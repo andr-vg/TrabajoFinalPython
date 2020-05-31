2d392c1c9dd48465739475dc9711749cec194fa3
Julian Feregotti <julainferegotti96@gmail.com>
2020-05-31 17:30:03 -0300

    Implemente la nueva funcion de generar palabra

--- TrabajoFinalPython/scrabbleAR/JugadorPC.py
+++ TrabajoFinalPython/scrabbleAR/JugadorPC.py
@@ -8,6 +8,8 @@ class PC():
         self.long_tablero = long_tablero
         self.botones = botones
         self.puntos_por_letra = puntos_por_letra
+        self.dificultad = "facil"
+        self.tipo = "VB"
 
     def setFichas(self, fichas_nuevas):
         self.fichas = fichas_nuevas
@@ -43,12 +45,47 @@ class PC():
                 return True
         return False
        
-    def _obtenerPalabra(self,long_max):
-        palabra = ""
-        lista_palabras = []
-        lista_fichas = list(self.fichas.values()) #Hago lista de las fichas
-        self._recursividadPalabras(lista_fichas,long_max,palabra,lista_palabras)
-        return "" if len(lista_palabras) == 0 else max(lista_palabras)
+    def _obtenerPalabra(self):
+        import itertools as it
+        from pattern.text.es import verbs, spelling, lexicon , parse
+        """
+        obtiene una palabra a partir de las fichas
+        """
+        letras = ""
+        for letra in self.fichas.values():
+            print("LETRA;",letra)
+            letras += letra
+        letras = letras.lower()
+        l=[]
+        s=spelling.keys()
+        le=lexicon.keys()
+        for opcion in range(2,len(letras)+1): #iterar por la combinación
+            pals = it.combinations(letras,opcion)
+            for combinacion in pals:
+                for pal in combinacion:
+                    p = list(it.permutations(combinacion))
+                    for f in p:
+                        evaluation = "".join(f)
+                        if evaluation in s and evaluation in le:
+                            l.append(evaluation)
+
+        l=list(set(l))
+        print("LISTA",l)
+        valido = False
+        palabra=max(l,key=lambda x: len(x))
+        if not (palabra.lower() in verbs) and (not palabra.lower() in spelling) and (not(palabra.lower() in lexicon) and not(palabra.upper() in lexicon) and not(palabra.capitalize() in lexicon)):
+            print("la palabra no existe")
+        else:
+            tipo_palabra = parse(palabra)
+            if (self.dificultad == "facil"):
+                valido = ("NN" in tipo_palabra) or ("VB" in tipo_palabra) or ("JJ" in tipo_palabra)
+            elif (self.dificultad == "medio"):
+                valido =  ("VB" in tipo_palabra) or ("JJ" in tipo_palabra)
+            else:
+                if self.tipo in tipo_palabra:   #Tipo seria un string que le se asigna aleatoreamente el tipo de una lista donde esta "NN" "JJ" y "VB"
+                    valido = True
+        if (valido):
+            return palabra
 
     def _mapear_tablero(self,posiciones_ocupadas_tablero, long_tablero):
         """ Esta funcion recibe las posiciones ocupadas en el tablero, mapea lugares disponibles y los devuelve
@@ -87,25 +124,25 @@ class PC():
         else:
             return long_max_tablero    
 
-    def _recursividadPalabras(self,lista,long_max,palabra,lista_palabras):
-        """
-        Agrega a lista_palabras las palabras que considera validas formadas por elementos de lista
-        """
-        for elem in lista:
-            #print("ELEMENTOOOOOO: ",elem)
-            palabra = palabra + elem
-            if len(palabra) > 1 and not palabra in lista_palabras and self._tiene_vocales(palabra) and self._tiene_consonantes(palabra) and es.palabra_valida(palabra):
-                lista_palabras.append(palabra)
-            lista_reducida =  lista.copy()
-            lista_reducida.remove(elem)
-            self._recursividadPalabras(lista_reducida, long_max, palabra, lista_palabras)
-            palabra = palabra[:len(palabra)-1]
+    # def _recursividadPalabras(self,lista,long_max,palabra,lista_palabras):
+        # """
+        # Agrega a lista_palabras las palabras que considera validas formadas por elementos de lista
+        # """
+        # for elem in lista:
+        #     #print("ELEMENTOOOOOO: ",elem)
+        #     palabra = palabra + elem
+        #     if len(palabra) > 1 and not palabra in lista_palabras and self._tiene_vocales(palabra) and self._tiene_consonantes(palabra) and es.palabra_valida(palabra):
+        #         lista_palabras.append(palabra)
+        #     lista_reducida =  lista.copy()
+        #     lista_reducida.remove(elem)
+        #     self._recursividadPalabras(lista_reducida, long_max, palabra, lista_palabras)
+        #     palabra = palabra[:len(palabra)-1]
  
     def jugar(self,window,posiciones_ocupadas_tablero):
         long_y_posiciones = self._mapear_tablero(posiciones_ocupadas_tablero,self.long_tablero) # obtenemos las posiciones libres en el tablero
         long_max_tablero = max(long_y_posiciones.keys()) # calculamos la long maxima entre todas esas posiciones libres
         long_max = self._calcular_long_maxima(long_max_tablero, len(self.fichas)) # nos quedamos con la max entre casillas y cant fichas
-        mejor_palabra = self._obtenerPalabra(long_max)  # obtenemos la mejor palabra posible
+        mejor_palabra = self._obtenerPalabra()  # obtenemos la mejor palabra posible
         if mejor_palabra != '':  # caso en que encuentra una palabra valida
             posiciones_random = random.randint(0, len(long_y_posiciones[long_max_tablero])-1)
             inicio_columna = random.randint(0,(long_max_tablero-len(mejor_palabra)))
